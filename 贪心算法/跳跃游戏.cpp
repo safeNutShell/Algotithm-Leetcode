bc3cfa663c4b8b2f9ed5d5d3443f4318b1c941ae
SK.Wang <46984866+safeNutShell@users.noreply.github.com>
2020-03-10 23:14:30 +0800

    Update 跳跃游戏.cpp

--- 贪心算法/跳跃游戏.cpp
+++ 贪心算法/跳跃游戏.cpp
@@ -4,21 +4,38 @@
 
 //解：
 bool canJump(vector<int>& nums) {
-    int pre=0,curr=0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==0){
-            pre=curr;
-            curr=i;
-            int tmp=curr-1;
-            while(1){
-                if(tmp<pre){
-                    return false;
+    if (nums.size() == 1) return true;
+    int curr = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 0) {
+            if (i == nums.size() - 1) {
+                curr = i;
+                int tmp = curr - 1;
+                while (1) {
+                    if (tmp < 0) {
+                        return false;
+                    }
+                    if (nums[tmp] < curr - tmp) {
+                        tmp--;
+                    }
+                    else {
+                        break;
+                    }
                 }
-                if(nums[tmp]<=curr-tmp){
-                    tmp--;
-                }
-                else{
-                    break;
+            }
+            else {
+                curr = i;
+                int tmp = curr - 1;
+                while (1) {
+                    if (tmp < 0) {
+                        return false;
+                    }
+                    if (nums[tmp] <= curr - tmp) {
+                        tmp--;
+                    }
+                    else {
+                        break;
+                    }
                 }
             }
 
@@ -26,3 +43,9 @@ bool canJump(vector<int>& nums) {
     }
     return true;
 }
+
+/*
+目前算法的问题在于对最后一位的特殊性没有照顾好，使用了额外的if判断
+应当是可以用更简单的方法实现的
+解题思路是：导致跳跃失败的是nums[i]==0，那么遍历过程中一旦遇到零就要向前回溯，看是否有某一位的跳跃可以跃过这个0，否则跳跃在i位断开
+*/
