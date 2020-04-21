efa50149aa2d14dd974450354ec646ace776d550
SK.Wang <46984866+safeNutShell@users.noreply.github.com>
2020-04-21 20:27:03 +0800

    Update 环形链表II.cpp

--- 链表/环形链表II.cpp
+++ 链表/环形链表II.cpp
@@ -3,3 +3,38 @@
 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
 */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                break;
+        }
+        if(fast==NULL || fast->next==NULL){
+            return NULL;
+        }
+        ListNode *start=head;
+        while(start!=slow){
+            slow=slow->next;
+            start=start->next;
+        }
+        return slow;
+    }
+};
+
+/*
+典型的寻找链表的循环开始点
+与数组“寻找重复数字”是同一问题
+*/
