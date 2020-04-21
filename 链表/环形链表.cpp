3ae501ed4e85596174a3e365ac606d5eeeac65bf
SK.Wang <46984866+safeNutShell@users.noreply.github.com>
2020-04-21 20:17:24 +0800

    Create 环形链表.cpp

---
+++ 链表/环形链表.cpp
@@ -0,0 +1,34 @@
+/*
+给定一个链表，判断链表中是否有环。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                break;
+        }
+        if(fast==NULL || fast->next==NULL){
+            return false;
+        }
+        return true;
+    }
+};
+
+/*
+典型使用快慢指针的问题，也可以选择使用set记录已出现的值
+*/
