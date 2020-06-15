7e9ab6df79ba94671602886ce72f094bd804596b
SK.Wang <46984866+safeNutShell@users.noreply.github.com>
2020-06-15 21:23:37 +0800

    Create 判断子序列.cpp

---
+++ 贪心算法/判断子序列.cpp
@@ -0,0 +1,18 @@
+/*
+给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+*/
+bool isSubsequence(string s, string t) {
+    int ptrs=0;
+    int ptrt=0;
+    while(ptrt<t.length()){
+        if(t[ptrt]==s[ptrs]){
+            ptrs++;
+        }
+        ptrt++;
+    }
+    if(ptrs==s.length())
+        return true;
+    return false;
+}
